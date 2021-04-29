4dd703e1e925eee42df725f1bf9dd1b9c1186a28
thonnnny <anthonymedrano1715@gmail.com>
2021-04-29 12:27:08 -0600

    changing and adding dashboard to admin panel

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,23 @@
-//All rendering views control are here.
+// All rendering views controls are here
 const router = require('express').Router();
 
-router.get('/',(req, res) => {
-    res.send("Funciona we")
+router.get('/', (req, res) => {
+    res.send("Funcionando crack");
 });
+
+router.get('/login', (req, res) => {
+    res.render('auth/login');
+});
+
+router.get('/dashboard', (req, res) => {
+    res.render('dashboard/dashboard')
+});
+
+//error response
+/*
+app.use(function(req, res, next) {
+    res.status(404).render('404');
+});
+*/
 
 module.exports = router;
